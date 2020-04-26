84e1ec6e118c4d3c47d1df3481c2218abd4fcbd5
codemafia111 <yashtailor2000@gmail.com>
2020-04-26 17:55:38 +0530

    added

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var app = express();
 var bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({extended:true}))
 
-app.listen(3000, function() { 
+app.listen(process.env.PORT || 3000, function() { 
     console.log('server running on port 3000'); 
 } ) 
 app.set('view engine','ejs')
